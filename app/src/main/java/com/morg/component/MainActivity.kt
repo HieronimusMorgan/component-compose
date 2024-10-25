750bb00c5cb35ee75a7b19b78e944efdb75f534d
Hieronimus Fredy Morgan <hieronimus.fredy@nexsoft.co.id>
2024-10-25 14:32:25 +0700

    Add TextFieldComponent -Add Type Drop Down Phone Number abd Dropdown Text

--- app/src/main/java/com/morg/component/MainActivity.kt
+++ app/src/main/java/com/morg/component/MainActivity.kt
@@ -10,6 +10,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.morg.component.button.ButtonComponent
 import com.morg.component.textfield.TextFieldComponent
+import com.morg.component.textfield.TextInputType
 import com.morg.component.ui.theme.ComponentSize
 import com.morg.component.ui.theme.ComponentTheme
 import com.morg.component.ui.theme.ComponentType
@@ -43,8 +48,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
+    val scrollState = rememberScrollState()
     Column(
         modifier = modifier
+            .verticalScroll(scrollState)
             .fillMaxSize()
             .padding(16.dp)
     ) {
@@ -114,17 +121,107 @@ fun Greeting(modifier: Modifier = Modifier) {
             modifier = Modifier.fillMaxWidth(),
             label = "Button",
             componentType = ComponentType.TERTIARY,
-            componentSize = ComponentSize.LARGE
+            componentSize = ComponentSize.LARGE,
         )
         Spacer(modifier = Modifier.height(5.dp))
 
-        TextFieldComponent(componentSize = ComponentSize.SMALL)
-        Spacer(modifier = Modifier.height(5.dp))
-        TextFieldComponent(componentSize = ComponentSize.MEDIUM)
-        Spacer(modifier = Modifier.height(5.dp))
-        TextFieldComponent(componentSize = ComponentSize.LARGE)
+        TextFieldComponent(label = "Full Name",
+            value = "Hieronimus Fredy Morgan",
+            readOnly = true,
+            hint = "Enter your password",
+            componentSize = ComponentSize.LARGE,
+            onTextChanged = { /* handle text change */ })
         Spacer(modifier = Modifier.height(5.dp))
 
+        TextFieldComponent(label = "Full Name",
+            value = "Hieronimus Fredy Morgan",
+            readOnly = true,
+            hint = "Enter your password",
+            componentSize = ComponentSize.MEDIUM,
+            onTextChanged = { /* handle text change */ })
+        Spacer(modifier = Modifier.height(5.dp))
+
+        TextFieldComponent(label = "Full Name",
+            value = "Hieronimus Fredy Morgan",
+            readOnly = true,
+            hint = "Enter your password",
+            componentSize = ComponentSize.SMALL,
+            onTextChanged = { /* handle text change */ })
+        Spacer(modifier = Modifier.height(5.dp))
+
+        TextFieldComponent(label = "Password",
+            value = "",
+            textInputType = TextInputType.PASSWORD,
+            hint = "Enter your password",
+            componentSize = ComponentSize.LARGE,
+            onTextChanged = { /* handle text change */ })
+        Spacer(modifier = Modifier.height(5.dp))
+
+        TextFieldComponent(label = "Password",
+            value = "",
+            textInputType = TextInputType.PASSWORD,
+            hint = "Enter your password",
+            componentSize = ComponentSize.MEDIUM,
+            onTextChanged = { /* handle text change */ })
+        Spacer(modifier = Modifier.height(5.dp))
+
+        TextFieldComponent(label = "Password",
+            value = "",
+            textInputType = TextInputType.PASSWORD,
+            hint = "Enter your password",
+            componentSize = ComponentSize.SMALL,
+            onTextChanged = { /* handle text change */ })
+        Spacer(modifier = Modifier.height(5.dp))
+
+        TextFieldComponent(label = "Password",
+            value = "",
+            hint = "Enter your password",
+            prefixIconImages = Pair(Icons.Filled.Favorite, null),
+            onTextChanged = { /* handle text change */ })
+        Spacer(modifier = Modifier.height(5.dp))
+        TextFieldComponent(label = "Phone Number",
+            value = "",
+            textInputType = TextInputType.PHONE,
+            countryCode = "+6",
+            countryCodes = listOf("+11", "+22", "+33", "+44", "+55", "+66"),
+            onCountryCodeClick = { /* Handle country code selection */ },
+            onTextChanged = { newText -> /* Handle text input */ })
+        Spacer(modifier = Modifier.height(5.dp))
+
+        TextFieldComponent(
+            label = "Select an Option",
+            value = "",
+            componentSize = ComponentSize.SMALL,
+            textInputType = TextInputType.DROPDOWN,
+            dropdownOptions = listOf("Option 1", "Option 2", "Option 3"),
+            onDropdownOptionSelected = { selected ->
+            },
+            hint = "Choose from the list"
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+
+        TextFieldComponent(
+            label = "Select an Option",
+            value = "",
+            componentSize = ComponentSize.MEDIUM,
+            textInputType = TextInputType.DROPDOWN,
+            dropdownOptions = listOf("Option 1", "Option 2", "Option 3"),
+            onDropdownOptionSelected = { selected ->
+            },
+            hint = "Choose from the list"
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+
+        TextFieldComponent(
+            label = "Select an Option",
+            value = "",
+            componentSize = ComponentSize.LARGE,
+            textInputType = TextInputType.DROPDOWN,
+            dropdownOptions = listOf("Option 1", "Option 2", "Option 3"),
+            onDropdownOptionSelected = { selected ->
+            },
+            hint = "Choose from the list"
+        )
     }
 }
 
